85cdd775e32f73a116701aa8a5313efe512feeb7
Stefan Remer <stefan.remer@gmail.com>
2018-01-22 20:49:05 +0100

    new routes for rest server

--- router/router.go
+++ router/router.go
@@ -3,8 +3,9 @@ package router
 import (
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"github.com/steffbo/intercise/home"
+
+	"github.com/gorilla/mux"
 	"github.com/steffbo/intercise/interval"
 )
 
@@ -13,7 +14,9 @@ func Router() *mux.Router {
 	r := mux.NewRouter()
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	r.HandleFunc("/", home.Home).Methods("GET")
-	r.HandleFunc("/interval", interval.HandleRequest).Methods("GET")
-	r.HandleFunc("/interval/{id}", interval.HandleRequest).Methods("GET")
+	r.HandleFunc("/interval", interval.GetIntervals).Methods("GET")
+	r.HandleFunc("/interval/{id}", interval.GetInterval).Methods("GET")
+	r.HandleFunc("/interval/{id}", interval.CreateInterval).Methods("POST")
+	r.HandleFunc("/interval/{id}", interval.DeleteInterval).Methods("DELETE")
 	return r
 }
